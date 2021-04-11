0f1bb9b1c3f1a6a9b48db47ead973e8f8c7351ba
k23REN <kd1233602@st.kobedenshi.ac.jp>
2021-04-12 03:18:55 +0900

    view生成関数ヒント追加

--- OpenGLProject/Source/Math.h
+++ OpenGLProject/Source/Math.h
@@ -10,6 +10,31 @@ class Vector3;
 class Quaternion;
  void __vectorcall VecStore(Vector3* out, const __m128& in);
  void __vectorcall  QuatStore(Quaternion* out, const __m128& in);
+
+ //lookAtRH(vec3(0.0f, 3.0f, 5.0f), vec3(0.0f, 0.0f, 0.0f),vec3(0.0f, 1.0f, 0.0f));
+ //	template<typename T, qualifier Q>
+	//GLM_FUNC_QUALIFIER mat<4, 4, T, Q> lookAtRH(vec<3, T, Q> const& eye, vec<3, T, Q> const& center, vec<3, T, Q> const& up)
+	//{
+	//	vec<3, T, Q> const f(normalize(center - eye));
+	//	vec<3, T, Q> const s(normalize(cross(f, up)));
+	//	vec<3, T, Q> const u(cross(s, f));
+
+	//	mat<4, 4, T, Q> Result(1);
+	//	Result[0][0] = s.x;
+	//	Result[1][0] = s.y;
+	//	Result[2][0] = s.z;
+	//	Result[0][1] = u.x;
+	//	Result[1][1] = u.y;
+	//	Result[2][1] = u.z;
+	//	Result[0][2] =-f.x;
+	//	Result[1][2] =-f.y;
+	//	Result[2][2] =-f.z;
+	//	Result[3][0] =-dot(s, eye);
+	//	Result[3][1] =-dot(u, eye);
+	//	Result[3][2] = dot(f, eye);
+	//	return Result;
+	//}
+
 #define M_PI 3.14159265358979
 #define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
 #define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
